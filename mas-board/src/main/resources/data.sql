ad63ef4abb9b3089b186584280a50db582db4416
chlee <chlee4635@gmail.com>
2022-07-28 22:24:16 +0900

    #13 - 테스트 데이터를 data.sql추가
    https://www.mockaroo.com/

---
+++ mas-board/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert
+into
+    article
+(content, created_at, created_by, hashtag, modified_at, modified_by, title)
+values
+    ('new Content', '2022-07-28T21:48:50.620297700', 'chlee', '#spring', '2022-07-28T21:48:50.620297700', 'chlee', 'new article');
